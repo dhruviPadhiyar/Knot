d76910daa8151d92b35e3aa44162d2d5b90533dd
Dhruvi Padhiyar <dhruvi.padhiyar@aipxperts.com>
2024-03-05 22:33:56 +0530

    custom logger

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,26 +11,36 @@ use App\Models\Ticket;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        Log::channel('user_activity_log')->info('Home page');
+
         if (Auth::guest()) {
+
             return view('index');
         }
 
-        if(Auth::user()->email=="admin@test.com"){
+        if (Auth::user()->email == "admin@test.com") {
+
+            $user_name = auth()->check() ? auth()->user()->name : '';
+            $user_email = auth()->check() ? auth()->user()->email : '';
+            $user = ['name' => $user_name, 'email' => $user_email];
+            Log::channel('user_activity_log')->info('User details', $user);
+
             $events = Event::all();
-            $onGoing = Event::where("status_id",2)->get();
-            $upComing = Event::where("status_id",1)->get();
+            $onGoing = Event::where("status_id", 2)->get();
+            $upComing = Event::where("status_id", 1)->get();
             $users = User::all();
             $hosts = Host::all();
             $requests = EventRequests::all();
-            $pending = EventRequests::where("status",'pending')->get();
-            $apr = EventRequests::where('status','aprooved')->get();
+            $pending = EventRequests::where("status", 'pending')->get();
+            $apr = EventRequests::where('status', 'aprooved')->get();
             $notifications = Notification::where('receiverId', Auth::user()->id)->get();
-            return view('admin.dashboard',compact('events','onGoing','upComing','users','hosts','requests','pending','apr','notifications'));
+            return view('admin.dashboard', compact('events', 'onGoing', 'upComing', 'users', 'hosts', 'requests', 'pending', 'apr', 'notifications'));
         }
 
         // if ($this->middleware('admin')) {
@@ -46,6 +56,12 @@ class HomeController extends Controller
         //     return view('admin.dashboard', compact('events', 'onGoing', 'upComing', 'users', 'hosts', 'requests', 'pending', 'apr', 'notifications'));
         // }
         else {
+
+            $user_name = auth()->check() ? auth()->user()->name : '';
+            $user_email = auth()->check() ? auth()->user()->email : '';
+            $user = ['name' => $user_name, 'email' => $user_email];
+            Log::channel('user_activity_log')->info('User details', $user);
+
             $events = Event::with('price')->orderBy('start_Date', 'asc')->get();
             $notifications = Notification::where('receiverId', Auth::user()->id)->get();
             return view('user.home', compact('events', 'notifications'));
--- config/logging.php
+++ config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -125,6 +125,11 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+        'user_activity_log' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/user_activity.log'),
+            'level' => 'debug',
         ],
     ],
 
